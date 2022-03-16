71605718aa46402f455f6742648b4ad32782be0b
LYanniButler <99315881+LYanniButler@users.noreply.github.com>
2022-03-16 18:04:46 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -5,6 +5,7 @@ mpg
 # My name is LaSheeda Butler
 #The data source is the mpg data table
 # This is my coding that generates Boxplots for transmission types
+#I like pie
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
